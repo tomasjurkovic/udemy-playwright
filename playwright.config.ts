4b14e879ef436118857b323fcca46fb2076022ca
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-26 16:04:29 +0200

    Update playwright.config.ts
    changed video to retain on failure and screenshot only on failure mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,8 +10,8 @@ const config: PlaywrightTestConfig = {
         },
         actionTimeout: 15000,
         ignoreHTTPSErrors: true,
-        video: "off",
-        screenshot: "off",
+        video: 'retain-on-failure',
+        screenshot: "only-on-failure"
     },
     projects: [
         {
