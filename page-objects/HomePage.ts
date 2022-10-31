7c48bbec4b55eeace814eec7ecc0783efb25c959
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-10-31 19:29:00 +0100

    Update HomePage.ts
    added feedback tab's locator
     to homepage

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -5,12 +5,14 @@ export class HomePage {
     readonly page: Page
     readonly signInButton: Locator
     readonly searchInput: Locator
+    readonly feedbackTab: Locator
 
     // Init selectors using constructor:
     constructor(page: Page) {
         this.page = page
         this.signInButton = page.locator("#signin_button")
         this.searchInput = page.locator("#searchTerm")
+        this.feedbackTab = page.locator("#feedback")
     }
 
     // Define Home page's methods:
@@ -20,13 +22,16 @@ export class HomePage {
 
     async clickOnSignInButton() {
         await this.signInButton.click()
-
     }
 
     async searchFor(searchCriteria: string) {
         await this.searchInput.type(searchCriteria)
         // simulating using enter from keyboard
         await this.page.keyboard.press('Enter')
+    }
+
+    async clickOnFeedbackTab() {
+        await this.feedbackTab.click()
     }
 }
 
