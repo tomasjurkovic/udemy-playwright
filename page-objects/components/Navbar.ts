32c30cb0291864d9943d9d38f5b5d0ca9e5a3d5d
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-11-02 10:11:57 +0100

    Update Navbar.ts
    used switch so it is better for maintenance

--- page-objects/components/Navbar.ts
+++ page-objects/components/Navbar.ts
@@ -5,7 +5,7 @@ export class Navbar {
     readonly page: Page
     readonly accountSummaryTab: Locator
     readonly accountActivityTab: Locator
-    readonly transferFundTab: Locator
+    readonly transferFundsTab: Locator
     readonly payBillsTab: Locator
     readonly myMoneyTab: Locator
     readonly onlineStatemenetsTab: Locator
@@ -15,34 +15,35 @@ export class Navbar {
         this.page = page
         this.accountSummaryTab = page.locator("#account_summary_tab")
         this.accountActivityTab = page.locator("#account_activity_tab")
-        this.transferFundTab = page.locator("#transfer_funds_tab")
+        this.transferFundsTab = page.locator("#transfer_funds_tab")
         this.payBillsTab = page.locator('#pay_bills_tab')
         this.myMoneyTab = page.locator('#my_money_tab')
         this.onlineStatemenetsTab = page.locator('#online_statements_tab')
     }
 
     // Define navbar methods:
-    async clickOnAccountSummaryTab() {
-        await this.accountSummaryTab.click()
-    }
-
-    async clickOnAccountActivityTab() {
-        await this.accountActivityTab.click()
-    }
-
-    async clickOnTransferFundsTab() {
-        await this.transferFundTab.click()
-    }
-
-    async clickOnPayBillsTab() {
-        await this.payBillsTab.click()
-    }
-
-    async clickOnMyMoneyTab() {
-        await this.myMoneyTab.click()
-    }
-
-    async clickOnOnlineStatementsTab() {
-        await this.onlineStatemenetsTab.click()
+    async clickOnTab(tabname) {
+        switch (tabname) {
+            case "Account Summary":
+                await this.accountSummaryTab.click()
+                break
+            case "Account Activity":
+                await this.accountActivityTab.click()
+                break
+            case "Transfer Funds":
+                await this.transferFundsTab.click()
+                break
+            case "Pay Bills":
+                await this.payBillsTab.click()
+                break
+            case "My Money":
+                await this.myMoneyTab.click()
+                break
+            case "Online Statements":
+                await this.onlineStatemenetsTab.click()
+                break
+            default:
+                throw new Error("This tab does not exist...")
+        }
     }
 }
