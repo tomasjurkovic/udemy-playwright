0d7c572733a60104858de90960b9f59904ebfd01
tomas.jurkovic <tomas.jurkovic@catalina.com>
2022-11-04 11:46:47 +0100

    Update api.spec.ts

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -9,6 +9,13 @@ test.describe.parallel("API Testing", () => {
         expect(response.status()).toBe(200) // success status code
     })
 
+    test("Simple API Test - How to Parse JSON", async ({ request }) => {
+        const response = await request.get(`${baseUrl}/users/3`)
+        expect(response.status()).toBe(200)
+
+        const responseBody = JSON.parse(await response.text())
+    })
+
     test("Simple API Test - Assert Invalid Endpoint", async ({ request }) => {
         const response = await request.get(`${baseUrl}/users/definitelyNotExistingEndpoint`)
         expect(response.status()).toBe(404) // not found status code
