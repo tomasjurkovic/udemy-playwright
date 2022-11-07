d54c575049d8d17b922c1c6254f7f9cb8654efbe
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-11-07 10:21:21 +0100

    Update tips.spec.ts
    added test where is example of using test.skip

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -4,12 +4,18 @@ test.describe.only("Tips and tricks section", () => {
 
     test("TestInfo Object", async ({ page }, testInfo ) => {
         await page.goto("https://www.example.com")
-        console.log(testInfo)
 
+        // console.log(testInfo)
         // by adding testInfo after the page, I can get lot of info about my test
         // it can be printed to console or sent to some monitoring tools
-        // also I can extract only some info like this:
 
+        // also I can extract only some info like this:
         console.log(testInfo.title)
+    })
+
+    test("Test skipp browser", async ({ page, browserName }) => {
+        test.skip(browserName === "chromium", "Feature not ready in Chrome browser")
+        // by adding test.skip, this test will not run if browser is chrome, but it runs in all other browsers
+        await page.goto("https://www.example.com")
     })
 })
